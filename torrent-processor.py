b1055c7e52f7b9babc199e245a90648cef7520eb
Ujjobbágy, Attila <attila.ujjobbagy@tesco.com>
2021-05-06 14:38:00 +0200

    Transmission added

--- torrent-processor.py
+++ torrent-processor.py
@@ -1,15 +1,33 @@
-from os import close
+from os import close, scandir
+from pathlib import Path
 import xml.etree.ElementTree as ET
 import re
+from confuse.exceptions import NotFoundError
 import requests
+import confuse
+import subprocess
 
-tree = ET.parse('rss.php.xml')
-root = tree.getroot()
+mandatory_config_values = ['regexps', 'rss']
 
-regexp_list = [
-    'Greys\.Anatomy.*720p.*HUN',
-    'Joban\.Rosszban.*720p.*',
-    'Tricky.*2160p.*']
+def load_configuration():
+    config = confuse.Configuration('torrent-processor', __name__)
+    for cfg in mandatory_config_values:
+        try:
+            config[cfg].get()
+        except NotFoundError:
+            print('ERROR_CONFIG - Configuration value "' + cfg + '" not found.')
+            return None
+
+    return config
+
+def load_rss(rss_url):
+    root = None
+    try:
+        root = ET.fromstring(requests.get(rss_url, allow_redirects=True).content)
+    except Exception:
+        print('CANNOT_LOAD_RSS - ' + rss_url)
+
+    return root
 
 def get_filename_from_cd(cd):
     if not cd:
@@ -19,29 +37,69 @@ def get_filename_from_cd(cd):
         return None
     return fname[0]
 
-for item in root.findall('.*/item'):
-    title = item.findtext('title')
-    found = False
-    for regexp in regexp_list:
-        if re.match(regexp, title):
-            found = True
-            print('MATCH - ' + title)
-            url = item.findtext('link')
-            r = requests.get(url, allow_redirects=True)
-            if r.status_code == 200:
-                filename = get_filename_from_cd(r.headers.get('content-disposition'))
-                if filename:
-                    print('DOWNLOAD_STARTED - ' + filename)
-                    torrent_file = None
-                    try:
-                        torrent_file = open(filename, 'wb')
-                        torrent_file.write(r.content)
-                    except IOError:
-                        print('ERROR_UNABLE_TO_WRITE - ' + filename)
-                    finally:
-                        if torrent_file:
-                            torrent_file.close
-                else:
-                    print('ERROR_DOWNLOAD_NO_FILENAME - ' + title)
-            else:
-                print('ERROR_DOWNLOAD_' + str(r.status_code) + ' - ' + title + ' - ' + url)
+def is_duplicate(filename):
+    duplicate = False
+    for file in scandir('.'):
+        if file.is_file and file.name == filename:
+            print('SKIP_ALREADY_DOWNLOADED - ' + filename)
+            duplicate = True
+
+    return duplicate
+
+def download_torrent_file(url, title):
+    filename = None
+    downloaded = False
+
+    r = requests.get(url, allow_redirects=True)
+    if r.status_code == 200:
+        filename = get_filename_from_cd(r.headers.get('content-disposition'))
+        if not filename:
+            print('ERROR_DOWNLOAD_NO_FILENAME - ' + title)
+        elif not is_duplicate(filename):
+            print('DOWNLOAD_STARTED - ' + filename)
+            torrent_file = None
+            try:
+                torrent_file = open(filename, 'wb')
+                torrent_file.write(r.content)
+                downloaded = True
+            except IOError:
+                print('ERROR_UNABLE_TO_WRITE - ' + filename)
+            finally:
+                if torrent_file:
+                    torrent_file.close()     
+    else:
+        print('ERROR_DOWNLOAD_' + str(r.status_code) + ' - ' + title + ' - ' + url)
+
+    return filename, downloaded
+
+def download_torrent(filename):
+    try:
+        download_process = subprocess.run(['transmission-remote', 'localhost:9091', '--add', filename], stdout=subprocess.PIPE)
+        if download_process.returncode != 0:
+            print('ERROR_DOWNLOADING_TORRENT - ' + download_process.stdout)
+    except Exception:
+            print('ERROR_DOWNLOADING_TORRENT - ' + filename)
+
+def main():
+    config = load_configuration()
+    if not config:
+        return
+
+    root = load_rss(config['rss'].get())
+    if not root:
+        return
+
+    regexp_list = config['regexps'].get()
+    for item in root.findall('.*/item'):
+        title = item.findtext('title')
+        for regexp in regexp_list:
+            if re.match(regexp, title):
+                print('MATCH - ' + title)
+                url = item.findtext('link')
+                filename, downloaded = download_torrent_file(url, title)
+                if downloaded:
+                    download_torrent(filename)
+
+if __name__ == "__main__":
+    main()
+
